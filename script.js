c28211237cd8790c4e27d5e23ec877f2f4fc7071
Prateekkumar-repository <96733993+Prateekkumar-repository@users.noreply.github.com>
2022-05-05 16:21:44 +0530

    Users add and delete booking functionality added

--- script.js
+++ script.js
@@ -1,34 +1,105 @@
+// function saveToLocalStorage(event) {
+//     event.preventDefault();
+//     const name = event.target.userName.value;
+//     const email = event.target.email.value;
+//     const number = event.target.contact.value;
+//     const date = event.target.bookingDate.value;
+//     const slot = event.target.showSlot.value;
+
+//     // localStorage.setItem('name',name);
+//     // localStorage.setItem('email',email);
+//     // localStorage.setItem('number',number);
+//     // localStorage.setItem('date',date);
+//     // localStorage.setItem('slot', slot);
+
+//     const userDetailsObject = {
+//       name,
+//       email,
+//       number,
+//       date,
+//       slot
+//     };
+
+//     localStorage.setItem(userDetailsObject.email, JSON.stringify(userDetailsObject));
+
+//     showBookingsOnScreen(userDetailsObject);
+
+// }
+
+
+// function showBookingsOnScreen(user) {
+//     const parentNode=document.getElementById('userBookings');
+//     const childHTML = `<li> ${user.name} - ${user.email}- ${user.date}- ${user.slot}
+//         <button  oneClick=deleteUser ('${user.email}')  >Cancel Booking </button>
+
+
+//     </li>`;
+//     parentNode.innerHTML += childHTML;
+// }
+
+// function deleteUser(emailId) {
+
+//     localStorage.removeItem(emailId);   
+//     removeUserFromScreen(emailId);
+// }
+// function removeUserFromScreen(emailId) {
+//     const parentNode=document.getElementById('userBookings');
+//         const childNodeToBeDeleted = document.getElementById(emailId);
+//         parentNode.removeChild(childNodeToBeDeleted);
+// }
+
+
 function saveToLocalStorage(event) {
     event.preventDefault();
-    const name = event.target.userName.value;
-    const email = event.target.email.value;
-    const number = event.target.contact.value;
-    const date = event.target.bookingDate.value;
-    const slot = event.target.showSlot.value;
+       const name = event.target.userName.value;
+        const email = event.target.email.value;
+        const number = event.target.contact.value;
+        const date = event.target.bookingDate.value;
+        const slot = event.target.showSlot.value;
+    const obj = {
+        name,
+        email,
+        number,
+        date,
+        slot
+    }
+    localStorage.setItem(obj.email, JSON.stringify(obj))
+    showNewUserOnScreen(obj)
+}
 
-    // localStorage.setItem('name',name);
-    // localStorage.setItem('email',email);
-    // localStorage.setItem('number',number);
-    // localStorage.setItem('date',date);
-    // localStorage.setItem('slot', slot);
+window.addEventListener("DOMContentLoaded", () => {
+    const localStorageObj = localStorage;
+    const localstoragekeys  = Object.keys(localStorageObj)
 
-    const userDetailsObject = {
-      name,
-      email,
-      number,
-      date,
-      slot
-    };
+    for(var i =0; i< localstoragekeys.length; i++){
+        const key = localstoragekeys[i]
+        const userDetailsString = localStorageObj[key];
+        const userDetailsObj = JSON.parse(userDetailsString);
+        showNewUserOnScreen(userDetailsObj)
+    }
+})
 
-    localStorage.setItem(userDetailsObject.email, JSON.stringify(userDetailsObject));
+function showNewUserOnScreen(user){
+    const parentNode = document.getElementById('userBookings');
+    const childHTML = `<li id=${user.email}> ${user.name} - ${user.email}
+                            <button onclick=deleteUser('${user.email}')> Delete User </button>
+                         </li>`
 
-    showBookingsOnScreen(userDetailsObject);
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
+}
+
+// deleteUser('abc@gmail.com')
+
+function deleteUser(emailId){
+    console.log(emailId)
+    localStorage.removeItem(emailId);
+    removeUserFromScreen(emailId);
 
 }
 
+function removeUserFromScreen(emailId){
+    const parentNode = document.getElementById('userBookings');
+    const childNodeToBeDeleted = document.getElementById(emailId);
 
-function showBookingsOnScreen(user) {
-    const parentNode=document.getElementById('userBookings');
-    const childHTML = `<li> ${user.name} - ${user.email}- ${user.date}- ${user.slot} </li>`;
-    parentNode.innerHTML += childHTML;
+    parentNode.removeChild(childNodeToBeDeleted)
 }
